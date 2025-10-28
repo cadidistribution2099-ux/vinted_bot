8bcc76e4e92c4341a1958a572dbb5ec43c15f077
cadidistribution2099-ux <cadidistribution2099@gmail.com>
2025-10-28 03:30:55 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import tasks
+from discord.ext import commands
 import requests
 import json
 import os
@@ -80,3 +80,4 @@ async def on_ready():
 
 
 client.run(TOKEN)
+
